0cac3453d7218586d7d65d03bee7ae02fa02e2b7
Victor Gabriel Vieira Francisco <victorgabrielfrancisco@gmail.com>
2023-09-08 16:58:49 -0300

    feat: Create MongoDeleteRepository

---
+++ src/repositories/delete-user/mongo-delete-user.ts
@@ -0,0 +1,28 @@
+import { IDeleteUserRepository } from "../../controllers/delete-user/protocols";
+import { ObjectId } from "mongodb";
+import { MongoClient } from "../../database/mongo";
+import { User } from "../../models/user";
+
+export class MongoDeleteUserRepository implements IDeleteUserRepository {
+  async deleteUser(id: string): Promise<User> {
+    // Delete user from Mongo
+    const user = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .findOne({ _id: new ObjectId(id) });
+
+    if (!user) {
+      throw new Error("User not found");
+    }
+
+    const { deletecount } = await MongoClient.db
+      .collection("users")
+      .deleteOne({ _id: new ObjectId(id) });
+
+    if (deletecount === 0) {
+      throw new Error("User not deleted");
+    }
+
+    const { _id, ...res } = user;
+    return { id: _id.toHexString(), ...res };
+  }
+}
