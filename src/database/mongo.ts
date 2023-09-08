d0854f5380c70fa997d7aa53cd481dfabbad3824
Victor Gabriel Vieira Francisco <victorgabrielfrancisco@gmail.com>
2023-09-08 10:37:41 -0300

    feat: MongoConnection

---
+++ src/database/mongo.ts
@@ -0,0 +1,21 @@
+import { MongoClient as Mongo, Db } from "mongodb";
+
+export const MongoClient = {
+  client: undefined as unknown as Mongo,
+  db: undefined as unknown as Db,
+
+  async connect(): Promise<void> {
+    const url =
+      process.env.MONGODB_URL || "mongodb+srv://cluster0.qxuf1cc.mongodb.net";
+    const username = process.env.MONGODB_USERNAME;
+    const password = process.env.MONGODB_PASSWORD;
+
+    const client = new Mongo(url, { auth: { username, password } });
+    const db = client.db("users-db");
+
+    this.client = client;
+    this.db = db;
+
+    console.log("connected to mongodb!");
+  },
+};
--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,14 +1,17 @@
 import { IGetUsersRepository } from "../../controllers/get-user/protocols";
+import { MongoClient } from "../../database/mongo";
+import { User } from "../../models/user";
 
 export class MongoGetUsersRepositories implements IGetUsersRepository {
-  async getUsers() {
-    return [
-      {
-        firstName: "Victor",
-        lastName: "Gabriel",
-        email: "victorgarbielfrancisco@gmail.com",
-        password: "any_password",
-      },
-    ];
+  async getUsers(): Promise<User[]> {
+    const users = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .find({})
+      .toArray();
+
+    return users.map(({ _id, ...rest }) => ({
+      ...rest,
+      id: _id.toHexString(),
+    }));
   }
 }
